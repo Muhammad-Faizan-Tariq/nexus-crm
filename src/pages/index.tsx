e4965ee5cc9d1c95cb8baf7a8a7ac7054dcfe776
Muhammad Faizan Tariq <mfaizantariq99@gmail.com>
2025-04-23 15:29:57 +0500

    feat: integrate Unified API TypeScript SDK for accounting functionality - and also console the response data - and data is coming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 
+import { UnifiedTo } from "@unified-api/typescript-sdk";
+
+async function run() {
+  const sdk = new UnifiedTo({
+    security: {
+      jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODA4MzViNDhiZDZjNGNmM2M2MWEzODIiLCJ3b3Jrc3BhY2VfaWQiOiI2ODA4MzViNDhiZDZjNGNmM2M2MWEzODUiLCJpYXQiOjE3NDUzNjg1MDB9.WoDcX0pmufeJICQBc8cTUfXeG8fegJX0CudeIqmswdo",
+    },
+  });
+
+  const res = await sdk.accounting.listAccountingAccounts({
+    connectionId: "680837b28bd6c4cf3c61a44d",
+  });
+
+  console.log("res >>>>>>>>>> ", res)
+}
+
+run();
+
 const index = () => {
+  console.log("UNIFIED_QUICKBOOK_CONNECTION_ID >>>>>>>>>")
   return <div className="text-blue-400 underline">Nexus CRM</div>;
 };
 
